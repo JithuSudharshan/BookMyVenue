269343e4a16484fe477a97ecb8b151f7ea95e498
Jithu Sudharshan <jithusudharshan@gmail.com>
2026-06-05 00:24:18 +0530

    Initial project setup

---
+++ server/src/app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+// Import your routes here
+// app.use('/api/users', userRoutes);
+
+export default app;
