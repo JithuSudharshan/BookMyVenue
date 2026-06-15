d1ae28a7f2ed28997099b740d2e272fb89eed5fd
Suhaib Nadakkavil <suhaibmd268@gmail.com>
2026-06-15 07:47:02 +0530

    feat(discovery): add venue listing API

--- server/src/routes/user/index.js
+++ server/src/routes/user/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 
 import homeRoutes from "./home.route.js";
+import venueRoutes from "./venues.route.js"
 
 const router = express.Router();
 
 router.use("/home", homeRoutes);
+router.use("/venues", venueRoutes);
 
 export default router;
